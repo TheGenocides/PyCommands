90d40c5c2f5858c935dc9a73f1d3b6c2b71e80b8
TheGenocide <82718066+TheGenocides@users.noreply.github.com>
2021-10-02 09:27:40 +0000

    BugFixed

--- PyCommands/commands.py
+++ PyCommands/commands.py
@@ -1,10 +1,19 @@
 import asyncio
 from .core import Command
 from typing import Callable, Type, Set
+from colorama import Fore, Back, Style
+
+
+SUCCESS=200
+DISABLED=69
+UN_COMMAND=50
+FAILED=30
+EXIT=0
 
 class BaseCommandMaker():
 	def __init__(self, prefix: str, **kwargs):
 		self.prefix = prefix
+		self.name=kwargs.get("name") or None
 		self.description=kwargs.get("description") or None
 		self._commands_ = {}
 
@@ -19,7 +28,7 @@ class BaseCommandMaker():
 
 	def command(self, name: str, cls: Type[Command] = Command, **kwargs) -> Callable:
 		def decorator(func) -> Command:
-			result = cls(func, name=name, **kwargs)
+			result = cls(name, func, **kwargs)
 			self._register_command(result)
 			return result
 		return decorator
@@ -27,21 +36,30 @@ class BaseCommandMaker():
 	def get_command(self, name: str) -> Command:
 		result=self._commands_.get(name)
 		return result or None
-	
-
 
 	def run(self):
 		async def run_():
+			print(Fore.GREEN + f"[{SUCCESS}]: Successfully Online As {self.name}. Using PyCommands Version 0.1.0!")
 			while True:
-				cmd: str = input("> ")
-				if not cmd.startswith(self.prefix):
-					print(f"Unknown prefix: {cmd[0]}")
+				cmd: str = input(Fore.GREEN + f"[{SUCCESS}]: " + Fore.BLUE + ">>> ")
+				if not cmd.startswith(self.prefix) or cmd is self.prefix: 	
+					if cmd.lower() == "exit()":
+						print(Fore.RED + f"[{EXIT}] Exited code, Status: None")
+						break
+				
 				else:
-					command=self.get_command(cmd.replace(self.prefix, " ").strip())
+					command=self.get_command(cmd.strip(self.prefix))
 					if not command:
-						print("Command not found!")
+						print(Fore.YELLOW + f"[{UN_COMMAND}]: " + Fore.RED + f"Unknown Command: {cmd}"
+					)
 					else:
-						await command.func()
+						if command.disabled is True:
+							print(Fore.YELLOW + f"[{DISABLED}]: " + Fore.RED + f"Disabled Command: {cmd}")
+							
+						else:
+							await command.func()
+						
+						
 		asyncio.run(run_())
 			
 class CommandMaker(BaseCommandMaker):
