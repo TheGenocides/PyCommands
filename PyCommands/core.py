61c728d9cf4d45a36c5b22f5efad7487611cad5e
TheGenocide <>
2021-10-05 01:48:21 +0000

    Added new internal command class and new invoke function

--- PyCommands/core.py
+++ PyCommands/core.py
@@ -1,12 +1,37 @@
+import asyncio
+from typing import List, Callable
+
 class Command():
-    def __init__(self, name, func, **fields):
+    def __init__(self, name: str, *, description: str = None, func: Callable, **kwargs):
         self.name = name
+        self.description = description or None
         self.func = func
-        self.description = fields.get("description") or None
-        self.disabled = fields.get("disabled") or False
+        self.disabled = kwargs.get("disabled") or False
         
     def __repr__(self):
         return "<Command: <name: {0.name} description: {0.description} disabled: {0.disabled} func: {0.func}>>".format(self)
 
     def disable(self):
         self.disabled = True
+
+    async def invoke(self, *args):
+        try:
+            if asyncio.iscoroutinefunction(self.func):
+                await self.func(*args)
+            else:
+                self.func(*args)
+        except TypeError:
+            if asyncio.iscoroutinefunction(self.func):
+                await self.func()
+            else:
+                self.func()
+
+class InternalCommand(Command): #This is for internal command like exit etc. You should be using class Command(): for making Command
+    def __init__(self, name: str, description: str, func: List[Callable]):
+        super().__init__(name, description=description, func=func, disabled=False)
+        self.name = name
+        self.description = description or None
+        self.func = func
+
+    def  __repr__(self):
+        return "<InternalCommand: <name: {0.name} description: {0.description} func: {0.func}>>".format(self)
