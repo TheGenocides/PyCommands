9f8efd1df6a6f210077f7cfea73d927cbd361852
TheGenocide <>
2021-10-05 01:55:13 +0000

    Fixed circular import error with TYPE_CHECKING

--- PyCommands/__init__.py
+++ PyCommands/__init__.py
@@ -1,6 +1,9 @@
-from .commands import *
-from .constants import *
-from .core import *
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+	from .commands import *
+	from .constants import *
+	from .core import *
 
 __title__ = "PyCommands"
 __version__="0.2.5"
