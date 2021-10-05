9fb8126bcb6d7a93ba9cb3bebbc1da640d275de4
TheGenocide <>
2021-10-05 08:19:02 +0000

    Added new Color class, update invoke function that lets you able to use typehinted, only str and int at the moment

--- PyCommands/core.py
+++ PyCommands/core.py
@@ -1,5 +1,8 @@
 import asyncio
+import random as ran
+from colorama import Fore
 from typing import List, Callable
+from .constants import TOO_MANY_ARGUMENTS, FAILED
 
 class Command():
     def __init__(self, name: str, *, description: str = None, func: Callable, **kwargs):
@@ -15,16 +18,29 @@ class Command():
         self.disabled = True
 
     async def invoke(self, *args):
+        types=list(self.func.__annotations__.values())
+        for arg in args:
+            for type in types:
+                try:
+                    if type(arg) == type: #Check if you can make a string to integer, if not it will raised ValueError then it'll get catch with except. 
+                        ...
+ 
+                except ValueError as error:
+                    print(Color.yellow() + f"[{FAILED}]: " + f"Failed to invoke '{self.name}': it raised an exception: {error}")
+                    return
         try:
             if asyncio.iscoroutinefunction(self.func):
                 await self.func(*args)
             else:
                 self.func(*args)
         except TypeError:
-            if asyncio.iscoroutinefunction(self.func):
-                await self.func()
-            else:
-                self.func()
+            try:
+                if asyncio.iscoroutinefunction(self.func):
+                    await self.func()
+                else:
+                    self.func()
+            except TypeError:
+                print(Color.yellow() + f"[{TOO_MANY_ARGUMENTS}]: Too many arguments passed for command '{self.name}': {' '.join(args)}")            
 
 class InternalCommand(Command): #This is for internal command like exit etc. You should be using class Command(): for making Command
     def __init__(self, name: str, description: str, func: List[Callable]):
@@ -35,3 +51,42 @@ class InternalCommand(Command): #This is for internal command like exit etc. You
 
     def  __repr__(self):
         return "<InternalCommand: <name: {0.name} description: {0.description} func: {0.func}>>".format(self)
+
+class Color():
+	@classmethod
+	def red(cls):
+		return Fore.RED
+	
+	@classmethod
+	def blue(cls):
+		return Fore.BLUE
+	
+	@classmethod
+	def yellow(cls):
+		return Fore.YELLOW
+	
+	@classmethod
+	def green(cls):
+		return Fore.GREEN
+	
+	@classmethod
+	def magenta(cls):
+		return Fore.MAGENTA
+	
+	@classmethod
+	def cyan(cls):
+		return Fore.CYAN
+	
+	@classmethod
+	def white(cls):
+		return Fore.WHITE
+	
+	@classmethod
+	def colors(cls):
+		return [cls.red(), cls.blue(), cls.green(), cls.yellow(), cls.magenta(), cls.cyan(), cls.white()]
+	
+	@classmethod
+	def random(cls):
+		return ran.choice(cls.colors())
+
+Colour=Color
